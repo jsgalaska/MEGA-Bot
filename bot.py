d2e61d3c3172c5118ccbb52634aaf67cc6bee0c3
jsgalaska <jsgalaska@gmail.com>
2015-07-07 11:28:58 -0400

    Reworked program, added simple response
    Functions the same, but is implemented different from the previous
    version. Added a test feature to see if the bot could respond to a
    message in chat.

--- bot.py
+++ bot.py
@@ -1,35 +1,117 @@
 # bot.py
 
-"""import cfg"""
 import socket
 import re
-import time
 
 HOST = "irc.twitch.tv"              # the Twitch IRC server
 PORT = 6667                         # always use port 6667!
-NICK = "shuabot"            # your Twitch username, lowercase
+NICK = "nick"            # your Twitch username, lowercase
 PASS = "oauth:" # your Twitch OAuth token
-CHAN = "#xshadow421x"                   # the channel you want to join
-RATE = (20.0/30) # messages per second
-CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+CHAN = "#chan"                   # the channel you want to join
 
-# network functions go here
+
+#-------------------------------------------------
+
+def send_pong(msg):
+    s.send(bytes('PONG %s\r\n' % msg, 'UTF-8'))
+
+def send_message(chan, msg):
+    s.send(bytes('PRIVMSG %s :%s\r\n' % (chan, msg), 'UTF-8'))
+
+def send_nick(nick):
+    s.send(bytes('NICK %s\r\n' % nick, 'UTF-8'))
+
+
+def send_pass(password):
+    s.send(bytes('PASS %s\r\n' % password, 'UTF-8'))
+
+
+def join_channel(chan):
+    s.send(bytes('JOIN %s\r\n' % chan, 'UTF-8'))
+
+
+def part_channel(chan):
+    s.send(bytes('PART %s\r\n' % chan, 'UTF-8'))
+
+def command_yolo():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'swag'), 'UTF-8'))
+
+def command_test():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'test'), 'UTF-8'))
+
+
+#------------------------------------------------
+
+def get_sender(msg):
+    result = ""
+    for char in msg:
+        if char == "!":
+            break
+        if char != ":":
+            result += char
+    return result
+
+
+def get_message(msg):
+    result = ""
+    i = 3
+    length = len(msg)
+    while i < length:
+        result += msg[i] + " "
+        i += 1
+    result = result.lstrip(':')
+    return result
+        
+def parse_message(msg):
+    if len(msg) >= 1:
+        msg = msg.split(' ')
+        options = {'!yolo': command_yolo,
+                   '!test': command_test,
+                   }
+        if msg[0] in options:
+            options[msg[0]]()
+
+#------------------------------------------------------
+data = ""
 
 s = socket.socket()
 s.connect((HOST, PORT))
-s.send("PASS {}\r\n".format(PASS).encode("utf-8"))
-s.send("NICK {}\r\n".format(NICK).encode("utf-8"))
-s.send("JOIN {}\r\n".format(CHAN).encode("utf-8"))
+
+send_pass(PASS)
+send_nick(NICK)
+join_channel(CHAN)
+
+print('loading')
 
 while True:
-    response = s.recv(1024).decode("utf-8")
-    if response == "PING :tmi.twitch.tv\r\n":
-        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
-    else:
-        print (response)
-        username = re.search(r"\w+", response).group(0) # return the entire match
-        message = CHAT_MSG.sub("", response)
-        print(username + ": " + message)
-    time.sleep(1 / RATE)
     
+    try:
+        data = data+s.recv(1024).decode('UTF-8')
+        print (data)
+        data_split = re.split(r"[~\r\n]+", data)
+        data = data_split.pop()
+
+        for line in data_split:
+            line = str.rstrip(line)
+            line = str.split(line)
+
+            if len(line) >= 1:
+                if line[0] == 'PING':
+                    send_pong(line[1])
+
+                if line[1] == 'PRIVMSG':
+                    sender = get_sender(line[0])
+                    message = get_message(line)
+                    parse_message(message)
+
+                    print(sender + ": " + message)
+
+
+        
+                    
+    except socket.error:
+        print("Socket lost")
+
+    except socket.timeout:
+        print("Socket timeout")
 
