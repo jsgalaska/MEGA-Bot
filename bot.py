8c5c67ebe3ac11321dc6106b1ec503249aded4fa
DAMedina <vipergtsr009@hotmail.com>
2017-04-19 19:39:05 -0400

    Reorganized code & general fixes/changes

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ adminList = [cfg.ADMIN1, cfg.ADMIN2, cfg.ADMIN3]
 sec = cfg.sec # ◄ Set desired seconds to wait for script termination (0 is 1 sec)
 MAXSENDINTERVAL = 20.0/30
 
-#------------------------------------------------▼
+#------------------------------------------------▼ IRC control dictionary
 
 def send_pong(msg):
     s.send(bytes('PONG %s\r\n' % msg, 'UTF-8'))
@@ -22,61 +22,14 @@ def send_message(chan, msg):
 def send_nick(nick):
     s.send(bytes('NICK %s\r\n' % nick, 'UTF-8'))
 
-
 def send_pass(password):
     s.send(bytes('PASS %s\r\n' % password, 'UTF-8'))
-
 
 def join_channel(chan):
     s.send(bytes('JOIN %s\r\n' % chan, 'UTF-8'))
 
-
 def part_channel(chan):
     s.send(bytes('PART %s\r\n' % chan, 'UTF-8'))
-
-def capreq_tags():
-    s.send(bytes("CAP REQ :twitch.tv/tags\r\n", 'UTF-8'))
-
-def capreq_membership():
-    s.send(bytes("CAP REQ :twitch.tv/membership\r\n", 'UTF-8'))
-
-def capreq_commands():
-       s.send(bytes("CAP REQ :twitch.tv/commands\r\n", 'UTF-8'))
-
-#------------------------------------------------▼ !commands dictionary
-
-def command_yolo():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'SWAG'), 'UTF-8'))
-
-def command_swag():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'YOLO'), 'UTF-8'))
-
-def command_clear():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, '.clear'), 'UTF-8'))
-
-def command_leave():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'OK, fine. Later Scrublords!'), 'UTF-8'))
-    time.sleep(1)
-    countdown(sec)
-
-def command_scrublord():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'Once a scrublord always a scublord'), 'UTF-8'))
-
-def command_purge(sender):
-    s.send(bytes("PRIVMSG %s :%s %s 1\r\n" %(CHAN, '.timeout', sender), 'UTF-8'))
-
-def general_commands(): #◄ 003 ▓
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'General commands available: !roulette, and !commands'), 'UTF-8'))
-
-#------------------------------------------------▼ Cap Commands
-
-def command_getusers():
-    s.send(bytes("CLEARCHAT %s" % CHAN, 'UTF-8'))
-
-#------------------------------------------------▼ Messages
-
-def arrive_message():
-    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'The Bot has arrived!'), 'UTF-8'))
 
 #------------------------------------------------▼ get_stuff
 
@@ -100,13 +53,67 @@ def get_message(msg):
     result = result.lstrip(':')
     return result
 
+#------------------------------------------------▼ !commands dictionary
+
+#------▼ Admin only commands
+
+def command_yolo():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'SWAG'), 'UTF-8'))
+
+def command_swag():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'YOLO'), 'UTF-8'))
+
+def command_clear():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, '.clear'), 'UTF-8'))
+
+def command_leave():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'OK, fine. Later Scrublords!'), 'UTF-8'))
+    time.sleep(1)
+    countdown(sec)
+
+def list_viewers(): # Prints logged viewers in Terminal/Shell
+    print('▌Viewers Log:')
+    for viewer in loggedViewers:
+        print('• ' + viewer)
+
+#------▼ Non-Admin commands
+
+def command_scrublord():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'Once a scrublord always a scublord'), 'UTF-8'))
+
+def command_general(): #◄ 003 ▓
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, '/me knows: !roulette, !commands, "How art thou <Bot Name>?"'), 'UTF-8'))
+
+#------▼ Soft Ban-Hammer
+
+def command_purge(sender):
+    s.send(bytes("PRIVMSG %s :%s %s 1\r\n" %(CHAN, '.timeout', sender), 'UTF-8'))
+
+#------------------------------------------------▼ Cap dictionary
+
+def command_getusers():
+    s.send(bytes("CLEARCHAT %s" % CHAN, 'UTF-8'))
+
+def capreq_tags():
+    s.send(bytes("CAP REQ :twitch.tv/tags\r\n", 'UTF-8'))
+
+def capreq_membership():
+    s.send(bytes("CAP REQ :twitch.tv/membership\r\n", 'UTF-8'))
+
+def capreq_commands():
+       s.send(bytes("CAP REQ :twitch.tv/commands\r\n", 'UTF-8'))
+
+#------------------------------------------------▼ Bot Messages
+
+def arrive_message():
+    s.send(bytes("PRIVMSG %s :%s\r\n" %(CHAN, 'The Bot has arrived!'), 'UTF-8'))
+
 #------------------------------------------------▼ The Bot know's that feel, bro
 
-#mood = [cfg.nice, cfg.fine, cfg.not_bad, cfg.ok, cfg.not_sure, cfg.frustrated, cfg.doin_great] 
+mood = [cfg.nice, cfg.fine, cfg.not_bad, cfg.ok, cfg.not_sure, cfg.frustrated, cfg.doin_great] 
 
-#def mood_swing():
-#    send_message(CHAN,"@"+sender+random.choice(mood))
-
+def mood_swing():
+    send_message(CHAN,"@"+sender+random.choice(mood))
 
 #------------------------------------------------▼ I wanna play a game
 
@@ -128,157 +135,115 @@ def shoot_me_mofo():
         send_message(CHAN, sender + ', you got lucky this time, scrub!')
         print('Bullet in:', chamber, 'of 6', 'Rotated to:', cylinder, 'of 6')
 
-#------------------------------------------------▼ Print list of viewers
+#------------------------------------------------▼ Message parsing & related variables
 
-def list_viewers():
-    print('Viewers Log:')
-    for viewer in loggedViewers:
-        print(viewer)
-
-#------------------------------------------------▼
-
-admin_commands = ['^!swag $', '^!c $', '^!viewers $'] #◄ 000 ▓ (◄ 003 Conflict with !c & !commands)
+admin_commands = ['^!swag $', '^!c $', '^!viewers $'] #◄ 000
+general_commands = ['^!yolo $', '^!roulette $', '^!commands $']
 href = ['https://www.', 'www.', '.com', '.co', '.uk', '.jpg', '.gif']#◄ 001
-part = '!exit' #◄ 002
+leave_channel = '!exit' #◄ 002
+scrublord = 'scrublord 4 life'
+hat = 'how art thou '+NICK+'?'
+
 
 def parse_message(sender, msg):
     if len(msg) >= 1:
-        s1 = 'scrublord 4 life'
-        hat = 'how art thou ' + NICK
-
         split_msg = msg.split(' ')
         
-        if s1 in msg.lower():
-            command_scrublord()
-        elif hat in msg.lower():
-            mood_swing()
-
-        elif part in msg.lower(): #◄ 002
-            for user in adminList:
-                if sender == user:
-                    command_leave()
-                    
-        
-
-
-        #----------------------------------------▼
-        # ▓ 2015-09-07 Test Code Below! ▓
-        link_privilege = None
-        print('DEBUG: link_privilege = ', link_privilege)
         #----------------------------------------▼ Admin !commands
-        #checks to see if sender is an admin
+        #Checks to see if sender is an admin
+        
         for command in admin_commands: #◄ 000
             if re.match(command, msg):
-                print('DEBUG: Match found in msg!')
+                adminFile = None
+                print('▓ Admin command detected! ▓')
                 try:
-                    aF = open('admins.txt', 'rt') # ◄ Create a/this user-list File
-                    with aF as file:
-                        print('DEBUG: Is Admins File open?: ', aF.closed)
-                        print('DEBUG: Scanning Sender data...')
-                        for line in file: 
+                    with open('admins.txt', 'rt') as adminFile:
+                        for line in adminFile: 
                             if sender in line:
-                                print('DEBUG: Sender is an Admin!')
-                                print('DEBUG: Scanning Options...')
-                                options = {'!swag': command_swag,
-                                           '!c': command_clear,
-                                           '!viewers': list_viewers
-                                           }
+                                options = {
+                                    '!swag': command_swag,
+                                    '!c': command_clear,
+                                    '!viewers': list_viewers
+                                    }
                                 if split_msg[0] in options:
                                     options[split_msg[0]]()
                                 
                 except IOError:
-                    print('DEBUG: ▓▓ ▓▓ ▓▓ ▓ ERROR ▓ File Not Found! ▓▓ ▓▓ ▓▓')
+                    print('▓▓ ▓▓ ERROR: Failed to load admins.txt! ▓▓ ▓▓')
                     return
                 
                 finally:
-                    if aF is not None:
-                        aF.close()
-                    print('DEBUG: Is Admins File open?: ', aF.closed)
+                    if adminFile is not None:
+                        adminFile.close()
                     return
 
-        #----------------------------------------▼
-        # ▓ 2015-09-07 Test Code Below! ▓
-        #if link posted, checks user's privilege     
+        #----------------------------------------▼ General Non-Admin commands
+        #If sender is not an Admin, runs this
+
+        for command in general_commands:
+            if re.match(command, msg):
+                print('▓ Command detected! ▓')
+                options = {
+                    '!yolo': command_yolo,
+                    '!roulette': shoot_me_mofo,
+                    '!commands': command_general
+                    }
+
+                if split_msg[0] in options:
+                    options[split_msg[0]]()
+                    return
+
+        #----------------------------------------▼ Link posting
+        #if link posted, checks user's privilege
+        
         for hlink in href: #◄ 001
             if hlink in msg.lower():
-                print('DEBUG: link_privilege = ', link_privilege)
-                print('DEBUG: Match found in msg!')
+                scrubsFile = None
+                link_privilege = None
+                print('▓ Hyperlink Detected!             ▓')
                 try:
-                    sF = open('scrubs.txt', 'rt') # ◄ Create a/this user-list File
-                    with sF as file:
-                        print('DEBUG: Scrub File open?: ', sF.closed)
-                        print('DEBUG: Scanning Sender data...')
-                        for line in file: 
+                    with open('scrubs.txt', 'rt') as scrubsFile:
+                        for line in scrubsFile: 
                             if sender in line:
-                                print('DEBUG: Sender is a Scrub!')
-                                print('DEBUG: link_privilege = ', link_privilege)
                                 link_privilege = 1
-                                print('DEBUG: link_privilege = ', link_privilege)
-                                
                 except IOError:
-                    print('DEBUG: ▓▓ ▓▓ ▓▓ ▓ ERROR ▓ File Not Found! ▓▓ ▓▓ ▓▓')
+                    print('▓▓ ▓▓ ERROR: Failed to load scrubs.txt! ▓▓ ▓▓')
                     return
                 
                 finally:
                     if link_privilege is None:
+                        print('▓ Scrub not found; purging sender ▓')
                         command_purge(sender)
-                        send_message(CHAN, 'You were not authorized to do that, ' + sender)
-                    if sF is not None:
-                        sF.close()
+                        send_message(CHAN, 'Sorry, you were not authorized to do that, ' + sender)
+                    if scrubsFile is not None:
+                        scrubsFile.close()
                     if link_privilege is not None:
                         link_privilege = None
-                    print('DEBUG: Scrub File open?: ', sF.closed)
                     return
 
-        #----------------------------------------▼
-        #if the sender is not an admin, runs this                
+        #----------------------------------------▼ Other message commands
+
+        if scrublord in msg.lower():
+            command_scrublord()
+        elif hat in msg.lower():
+            mood_swing()
+
+        elif leave_channel in msg.lower(): #◄ 002
+            for user in adminList:
+                if sender == user:
+                    command_leave()
+
         else:
-            print('DEBUG: No Match')
-            options = {'!yolo': command_yolo,
-                       '!roulette': shoot_me_mofo,# ◄, (Reminder to replace comma if solution found for !commands)
-                       '!commands': general_commands
-                       }
-            if split_msg[0] in options:
-                options[split_msg[0]]()
-                return
-
-        
-
-
-        ''' ▓ 2015-09-07 Temp Removal (Former Code) ▓
-        #----------------------------------------▼ !commands [Admins]
-        #checks to see if sender is an admin
-        for user in adminList:
-            if sender == user:
-                options = {'!yolo': command_yolo,
-                   '!swag': command_swag,
-                   '!c': command_clear,
-                   '!exit': command_leave,
-                   '!roulette': shoot_me_mofo
-                   }
-                if split_msg[0] in options:
-                    options[split_msg[0]]()
-                    return
-        #----------------------------------------▼                
-        #if the sender is not an admin, runs the command
-        hlink = 'http'
-        if hlink in msg:
-            command_purge(sender)
-            send_message(CHAN, 'You were not authorized to do that, ' + sender)
-        else:
-            options = {'!yolo': command_yolo,
-                       '!roulette': shoot_me_mofo
-                    }
-            if split_msg[0] in options:
-                options[split_msg[0]]()'''
+            return
 
 #------------------------------------------------▼ Terminate Script Timer
 
 def countdown(sec):
-    for viewer in loggedViewers:
+    #---------This CANNOT work here! #◄ 004
+    '''for viewer in loggedViewers:
         viewersLog.write(viewer)
         viewersLog.write('\n')
-    viewersLog.close()
+    viewersLog.close()'''
     print('Bot DISABLED!') 
     while (sec >= 0):
         print('Terminating script in:', sec,'seconds.')
@@ -300,7 +265,7 @@ send_pass(PASS)
 send_nick(NICK)
 capreq_membership()
 capreq_commands()
-#capreq_tags()
+#capreq_tags() -Works, but breaks adminList commands; possibly other things
 join_channel(CHAN)
 
 print('Initializing')
@@ -310,9 +275,12 @@ try:
     with open('viewers_log.txt', 'rt') as viewersLog:
         for line in loggedViewers:
             loggedViewers.append(line)
+        for viewer in loggedViewers: #◄ 004 May or may not work properly here
+            viewersLog.write(viewer)
+            viewersLog.write('\n')
+        viewersLog.close()
 except IOError:
-    print('Error opening viewers_log')
-
+    print('▓▓ ▓▓ ERROR: Failed to load viewers_log! ▓▓ ▓▓')
 
 while True:
     
@@ -334,23 +302,26 @@ while True:
                     sender = get_sender(line[0])
                     message = get_message(line)
                     parse_message(sender, message)
+                    print('CHAT> '+sender +": " + message)
 
-                    print(sender + ": " + message)
                 if line[1] == 'JOIN':
                     sender = get_sender(line[0])
                     loggedViewers.append(sender)
-                    send_message(CHAN, 'Welcome '+sender+'! Ya Scrub')
+                    print('▌VIEWER UPDATE: ' +'|' +sender +'|' +' has joined the chat!')
+                    #send_message(CHAN, 'Welcome '+sender+'! Ya Scrub') -Turned off. May scare/trigger people
+
+                if line[1] == 'PART':
+                    sender = get_sender(line[0])
+                    loggedViewers.append(sender)
+                    print('▌VIEWER UPDATE: '+sender +' has left the chat! :(')
 
             while ENGAGE == False:
                 print('I have arrived in ' + CHAN + "'s channel!")
-                arrive_message()
+                #arrive_message()
                 time.sleep(1)
                 ENGAGE = True
         time.sleep(MAXSENDINTERVAL)
-
-
-        
-                    
+                
     except socket.error:
         print("Socket lost")
 
